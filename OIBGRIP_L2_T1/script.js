b1e94a81cc693d992f4f1c7549b3e117e99f93ea
Sindhu c <105033159+19sindhu@users.noreply.github.com>
2023-03-08 21:08:26 +0530

    Update script.js

--- OIBGRIP_L2_T1/script.js
+++ OIBGRIP_L2_T1/script.js
@@ -1,21 +1,42 @@
-let input = document.getElementById('result');
-
-function insert(value) {
-  input.value += value;
+function insert(num) {
+  document.getElementById('result').value += num;
 }
 
 function clearScreen() {
-  input.value = '';
+  document.getElementById('result').value = '';
 }
 
 function backspace() {
-  input.value = input.value.slice(0, -1);
+  let result = document.getElementById('result').value;
+  document.getElementById('result').value = result.substring(0, result.length - 1);
 }
 
 function calculate() {
-  try {
-    input.value = eval(input.value);
-  } catch (error) {
-    input.value = 'Error';
+  let result = document.getElementById('result').value;
+  if (result) {
+      let operands = result.match(/[\d.]+/g);
+      let operators = result.match(/[^\d.]+/g);
+      let total = Number(operands[0]);
+      for (let i = 0; i < operators.length; i++) {
+          let operator = operators[i];
+          let operand = Number(operands[i + 1]);
+          switch (operator) {
+              case '+':
+                  total += operand;
+                  break;
+              case '-':
+                  total -= operand;
+                  break;
+              case '*':
+                  total *= operand;
+                  break;
+              case '/':
+                  total /= operand;
+                  break;
+              default:
+                  break;
+          }
+      }
+      document.getElementById('result').value = total;
   }
 }
